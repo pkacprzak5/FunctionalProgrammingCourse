f2471a730a0f6760e242b46e5ff90a8111f4d6bf
Piotr Kacprzak <piotrkac4@gmail.com>
2024-10-21 14:01:40 +0200

    Lab1 done

--- lab1/ex8.hs
+++ lab1/ex8.hs
@@ -6,3 +6,21 @@ not' b = case b of
 absInt n = case(n >= 0) of
     True -> n
     _ -> -n
+
+or' :: (Bool, Bool) -> Bool
+or' (a, b) = case(a || b) of
+    True -> False
+    _ -> True
+
+and' :: (Bool, Bool) -> Bool
+and' (a, b) = case(a && b) of
+    True -> False
+    _ -> True
+
+nand' :: (Bool, Bool) -> Bool
+nand' (a, b) = a && b
+
+xor' :: (Bool, Bool) -> Bool
+xor' (a, b) = case a of
+    True -> not' b
+    _ -> b
